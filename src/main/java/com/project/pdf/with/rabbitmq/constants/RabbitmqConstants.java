ebb0149220852a7b96116fab4d345179ee635dc0
Flavio F <flavio.win@gmail.com>
2024-11-20 20:23:36 -0300

    Exclude poc auxiliar files

--- src/main/java/com/project/pdf/with/rabbitmq/constants/RabbitmqConstants.java
+++
@@ -1,5 +0,0 @@
-package com.project.pdf.with.rabbitmq.constants;
-
-public class RabbitmqConstants {
-  public static final String FILA_ESTOQUE = "ESTOQUE";
-}
--- src/main/java/com/project/pdf/with/rabbitmq/dto/PdfDto.java
+++
@@ -1,8 +0,0 @@
-package com.project.pdf.with.rabbitmq.dto;
-
-public record PdfDto(Long id, String name, String link) {
-
-  public static PdfDto fromEntity(PdfDto pdfDto) {
-    return new PdfDto(pdfDto.id, pdfDto.name, pdfDto.link);
-  }
-}
