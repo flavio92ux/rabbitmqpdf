9ba78c0177469621c89407579ca628039e27d41c
Flavio F <flavio.win@gmail.com>
2024-11-20 13:14:11 -0300

    Create Database With Spring

--- src/main/java/com/project/pdf/with/rabbitmq/entity/MergedPdf.java
+++ src/main/java/com/project/pdf/with/rabbitmq/entity/MergedPdf.java
@@ -6,8 +6,15 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+@Getter
+@Setter
+@NoArgsConstructor
 @Entity
+@Table(name = "merged")
 public class MergedPdf {
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
